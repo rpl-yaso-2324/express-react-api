204e06710935e21a5e8ffb2a87d33b1cd88467f0
F18R10 <ramadhanfauzi932@gmail.com>
2023-10-23 15:38:23 +0700

    header

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route} from"react-router-dom";
+import Layout from "./components/layouts/layout";
+
+function App() {
+  return (
+  <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/layouts/Layout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+
+function Layout({ children }) {
+    return (
+        <>
+        <Header />
+        {children}
+        <Outlet />
+        </>
+    );
+}
+
+export default Layout;
