7c6d708369cdb1a7169fe6bfe5e57e279a7ce3fe
nazwakito <nazwaleony77@gmail.com>
2023-10-23 23:35:22 +0700

    dikit perubahan

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/layout";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout/>}></Route>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Header from "./header";
+
+function Layout({ children }) {
+	return (
+		<>
+			<Header />
+			{children}
+			<Outlet />
+		</>
+	);
+}
+
+export default Layout;
