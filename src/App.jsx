ca36a77d165f5609eda3af43ec2c92ea26625874
Rmt_47 <eighteenofmarch07@gmail.com>
2023-10-23 15:33:47 +0700

    menambahkan navbar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './layout/layout';
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
