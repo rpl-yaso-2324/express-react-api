12bd42cd941c8d3a73d8867db4cf2cdb6edab2a5
mfahri <xbx111xx@gmail.com>
2023-10-24 07:33:29 +0700

    nambah navbar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './layout/Layout';
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+      </>
+  )
+}
+
+export default App
