80e5e24e850dc8a62f879509d844be5da3ccd75a
yasrii <davinnaziel374@gmail.com>
2023-10-24 08:32:40 +0700

    atos

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layouts/Layout";
+
+function App() {
+   return (
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}></Route>
+      </Routes>
+    </BrowserRouter>
+   )
+
+}
+    
+export default App;
