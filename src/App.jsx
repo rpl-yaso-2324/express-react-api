b39d62978a6ef08f2a50a0620373e0b1f4644cee
SownAja123 <nahasonanganmuse@gmail.com>
2023-10-24 07:39:55 +0700

    atos

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layouts/Layout";
+
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
