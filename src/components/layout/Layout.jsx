8f7d4fa6cebfba79a39bf8995ec5cbe87618fb9c
xipins <pinayulyanti7@gmail.com>
2023-10-24 14:51:01 +0700

    react

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,14 @@
+import Header from "./header";
+import {Outlet} from "react-router-dom";
+
+function Layout({children}){
+    return (
+        <>
+        <Header/>
+        {children}
+        <Outlet/> 
+        </>
+    );
+}
+
+export default Layout;
