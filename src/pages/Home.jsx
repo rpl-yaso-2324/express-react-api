6cab0b82ff907bde2ff5da158d5f4901732b4c9b
rozannouval <rozannouval@gmail.com>
2023-10-25 08:32:07 +0700

    tadi rozan, lupa belum ganti user

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
                     <Card className="border-0 rounded shadow-sm">
                         <Card.Body className="p-4">
                         <h1>EXPRESS.JS + REACT.JS</h1>
-                        <p class="lead"><strong>rozannouval</strong> Still Learning</p>
+                        <p class="lead"><strong>rozannouval </strong> Still Learning</p>
                         <Button href="https://github.com/rozannouval" target="_blank" variant="primary" size="lg">Github</Button>
                         </Card.Body>
                     </Card>
