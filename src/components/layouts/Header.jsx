61ea73a6f5436cd698efb20f2ceab3c1ea3476fb
rabbanisan <bani.ramadhan99@gmail.com>
2023-10-25 10:49:38 +0700

    add pages,router,bootstrap

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -9,11 +9,15 @@ function Header(params) {
 				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 				<Navbar.Collapse id="responsive-navbar-nav">
 					<Nav className="me-auto">
-						<Nav.Link as={Link} to="/" className="nav-link">
-							HOME
+						<Nav.Link>
+							<Link to="/" className="nav-link">
+								HOME
+							</Link>
 						</Nav.Link>
-						<Nav.Link as={Link} to="/posts" className="nav-link">
-							POSTS
+						<Nav.Link>
+							<Link to="/posts" className="nav-link">
+								POSTS
+							</Link>
 						</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
