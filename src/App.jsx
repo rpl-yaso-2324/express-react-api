e672c4c66785e84e0d92077a8212c100d0bf707a
xipins <pinayulyanti7@gmail.com>
2023-10-25 08:27:39 +0700

    menambah navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/layout";
+
 function App () {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}></Route>
-    </Routes>
-  </BrowserRouter>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
