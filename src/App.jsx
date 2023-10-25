f027baf56207588f7528551547d7fce459a18c7e
fhanywikey <fhanywike9@gmail.com>
2023-10-25 08:31:52 +0700

    menambah folder posts pada pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/layout";
+import IndexPosts from "./components/pages/posts/indexPosts";
+import CreatePosts from "./components/pages/posts/createPosts";
+import EditPosts from "./components/pages/posts/editPosts";
 function App () {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}></Route>
-    </Routes>
-  </BrowserRouter>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+         <Route index element={<Home />}/>
+         <Route exact path="/posts" component={IndexPosts} />
+         <Route exact path="/posts/create" component={CreatePosts} />
+         <Route exact path="/posts/edit/:id" component={EditPosts} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -6,20 +6,26 @@ import {Link} from "react-router-dom";
 
 function Header() {
   return (
-    
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
           <Navbar.Brand to="/">EXPRESS.JS + REACT.JS</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
               <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
-                  <Nav.Link as={Link} to="/" className="nav-link">HOME</Nav.Link>
-                  <Nav.Link as={Link} to="/posts" className="nav-link">POSTS</Nav.Link>
+                  <Nav.Link>
+                    <Link to="/" className="nav-link">
+                      HOME
+                      </Link>
+                  </Nav.Link>
+                  <Nav.Link>
+                    <Link to="/posts" className="nav-link">
+                     POST
+                      </Link>
+                  </Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Container>
       </Navbar>
-
   );
 }
 export default Header;
