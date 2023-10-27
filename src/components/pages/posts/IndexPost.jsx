5a3bd1fd55b7e369cb4f0276220b2a332f39e7f2
fhanywikey <fhanywike9@gmail.com>
2023-10-27 11:15:56 +0700

    membuat postingan

--- src/components/pages/posts/IndexPost.jsx
+++ src/components/pages/posts/IndexPost.jsx
@@ -1,40 +1,67 @@
-//import component Bootstrap React 
-import { Card, Container, Row, Col } from 'react-bootstrap';
+//import hook useState dan useEffect from react
+import { useState, useEffect } from 'react';
 
-import React, { useState } from 'react';
+//import react router dom
+import { Link } from "react-router-dom";
+
+//import component Bootstrap React
+import { Card, Container, Row, Col, Button, Table } from 'react-bootstrap';
+
+//import axios
+import axios from 'axios';
 
 function IndexPost() {
-	const [ukuran, setUkuran] = useState('h1');
 
-	const UkuranH1 = () => {
-		setUkuran('h1');
-	};
-	
-	const UkuranH2 = () => {
-		setUkuran('h2');
-	};
-	
-	const UkuranH3 = () => {
-		setUkuran('h3');
-	};
+    //define state
+    const [posts, setPosts] = useState([]);
+
+    //useEffect hook
+    useEffect(() => {
+
+        //panggil method "fetchData"
+        fetchData();
+
+    }, []);
+
+    //function "fetchData"
+    const fetchData = async () => {
+        //fetching
+        const response = await axios.get('http://localhost:3000/api/postingan');
+        //get response data
+        const data = await response.data.data;
+
+        //assign response data to state "posts"
+        setPosts(data);
+    }
+
     return (
         <Container className="mt-3">
             <Row>
                 <Col md="{12}">
                     <Card className="border-0 rounded shadow-sm">
-                        <Card.Body className={`${ukuran}`}>
-                            HALAMAN INDEX POST
+                        <Card.Body>
+                            <Button as={Link} to="/posts/create" variant="success" className="mb-3">TAMBAH POST</Button>
+                            <Table striped bordered hover className="mb-1">
+                                <thead>
+                                    <tr>
+                                        <th>NO.</th>
+                                        <th>TITLE</th>
+                                        <th>CONTENT</th>
+                                        <th>AKSI</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    { posts.map((post, index) => (
+                                        <tr key={ post.id }>
+                                            <td>{ index + 1 }</td>
+                                            <td>{ post.title }</td>
+                                            <td>{ post.content }</td>
+                                            <td className="text-center"></td>
+                                        </tr>
+                                    )) }
+                                </tbody>
+                            </Table>
                         </Card.Body>
-    <div>
-      {Element === 'h1' && <h1>Ini Ukuran H1</h1>}
-      {Element === 'h2' && <h2>Ini Ukuran H2</h2>}
-      {Element === 'h3' && <h3>Ini Ukuran H3</h3>}
-      <button onClick={()=>UkuranH1()}>H1</button>
-      <button onClick={()=>UkuranH2()}>H2</button>
-      <button onClick={()=>UkuranH3()}>H3</button>
-    </div>
-
-
                     </Card>
                 </Col>
             </Row>
