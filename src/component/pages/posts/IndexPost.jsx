a4a66eed072c7820b34c8de893aed7c408709e63
MuhammadRafliF <rflfadilla@gmail.com>
2023-10-27 11:15:30 +0700

    fetchdata

--- src/component/pages/posts/IndexPost.jsx
+++ src/component/pages/posts/IndexPost.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react';
-import { Card, Container, Row, Col } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+//import { Link } from "react-router-dom";
+//import { Card, Container, Row, Col, Button, Table } from 'react-bootstrap';
+import axios from 'axios';
 
 function IndexPosts() {
-    const [ukuran, setUkuran] = useState("h1")
-    return (
-        <Container className="mt-3">
-            <Row md={6}>
-                <Col md="{12}">
-                    <Card className="border-0 rounded shadow-sm">
-                        <Card.Body>
-                            HALAMAN INDEX POST
-                        </Card.Body>
-                        <button className="btn btn-dark mt-2" onClick={()=> setUkuran("h1")}>
-                            <h1>h1</h1>
-                        </button>
-                        <button className="btn btn-dark mt-2" onClick={()=> setUkuran("h2")}>
-                            <h2>h2</h2>
-                        </button>
-                        <button className="btn btn-dark mt-2" onClick={()=> setUkuran("h3")}>
-                            <h3>h3</h3>
-                        </button>
-                        <h1>{ukuran}</h1>
-                
+    const [postingan, setPostingan] = useState([]);
 
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
-    );
+    useEffect(() => {
+       
+        fetchData();
+
+    }, []);
+
+    const fetchData = async () => {
+
+        const response = await axios.get("http://localhost:3000/api/postingan");
+
+        const dataPostingan = await response.data.postingan;
+
+        setPostingan(dataPostingan);
+
+        console.log(postingan);
+    };
+
+    return <></>
+
+
 }
 
 export default IndexPosts;
