b551f7ee7938c9c3b420e9f2905104d5ca89ed95
nazwakito <nazwaleony77@gmail.com>
2023-10-27 11:16:24 +0700

    commit index post

--- src/components/pages/posts/IndexPost.jsx
+++ src/components/pages/posts/IndexPost.jsx
@@ -1,31 +1,56 @@
-import {useState} from "react" ;
+//import component Bootstrap React
 import { Card, Container, Row, Col } from "react-bootstrap";
+//import hook useState dan useEffect from react
+import { useState, useEffect } from "react";
+
+//import axios
+import axios from 'axios';
 
 function IndexPost() {
-    const [ukuran, setUkuran] = useState("h1")
-	return (
-		<Container className="mt-3">
-			<Row md={6}>
-				<Col md="{12}">
-					<Card className="border-0 rounded shadow-sm">
-						<Card.Body>
-                            HALAMAN INDEX POST
-                        </Card.Body>
-                        <button className="btn btn-dark mt-2" onClick={() => setUkuran("h1")}>
-                           <h1>h1</h1>
-                        </button>
-                        <button className="btn btn-dark mt-2" onClick={() => setUkuran("h2")}>
-                           <h2>h2</h2>
-                        </button>
-                        <button className="btn btn-dark mt-2" onClick={() => setUkuran("h3")}>
-                           <h3>h3</h3>
-                        </button>
-                        <h1>{ukuran}</h1>
-					</Card>
-				</Col>
-			</Row>
-		</Container>
-	);
+  //define state
+	const [postingan, setPostingan] = useState([]);
+
+	//useEffect hook
+	useEffect(() => {
+		//panggil method "fetchData"
+		fetchData();
+	}, []);
+
+	const fetchData = async () => {
+		//fetching
+		const response = await axios.get("http://localhost:3000/api/postingan");
+		//get response data
+		const dataPostingan = await response.data.postingan;
+
+		//assign response data to state "posts"
+		setPostingan(dataPostingan);
+
+		console.log(postingan);
+	};
+	return <></>;
+
 }
 
 export default IndexPost;
+
+		// <Container className="mt-3">
+		// 	<Row md={6}>
+		// 		<Col md="{12}">
+		// 			<Card className="border-0 rounded shadow-sm">
+		// 				<Card.Body>
+      //                       <h5>HALAMAN INDEX POST</h5>
+      //                   </Card.Body>
+      //                   <button className="btn btn-dark mt-2" onClick={() => setUkuran("h1")}>
+      //                      <h1>h1</h1>
+      //                   </button>
+      //                   <button className="btn btn-dark mt-2" onClick={() => setUkuran("h2")}>
+      //                      <h2>h2</h2>
+      //                   </button>
+      //                   <button className="btn btn-dark mt-2" onClick={() => setUkuran("h3")}>
+      //                      <h3>h3</h3>
+      //                   </button>
+      //                   <h1>{ukuran}</h1>
+		// 			</Card>
+		// 		</Col>
+		// 	</Row>
+		// </Container>
