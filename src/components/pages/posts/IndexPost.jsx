374ab9ca72379e6c7650d768706137b3db3d1874
nazwakito <nazwaleony77@gmail.com>
2023-10-30 17:12:31 +0700

    commit index post lagii

--- src/components/pages/posts/IndexPost.jsx
+++ src/components/pages/posts/IndexPost.jsx
@@ -1,34 +1,73 @@
-//import component Bootstrap React
-import { Card, Container, Row, Col } from "react-bootstrap";
 //import hook useState dan useEffect from react
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
+
+//import react router dom
+import { Link } from "react-router-dom";
+
+//import component Bootstrap React
+import { Card, Container, Row, Col, Button, Table } from 'react-bootstrap';
 
 //import axios
 import axios from 'axios';
 
 function IndexPost() {
-  //define state
-	const [postingan, setPostingan] = useState([]); 
 
-	//useEffect hook
-	useEffect(() => {
-		//panggil method "fetchData"
-		fetchData();
-	}, []);
+    //define state
+    const [posts, setPosts] = useState([]);
 
-	const fetchData = async () => {
-		//fetching
-		const response = await axios.get("http://localhost:3000/api/postingan");
-		//get response data
-		const dataPostingan = await response.data.postingan;
+	console.log(posts);
+    //useEffect hook
+    useEffect(() => {
 
-		//assign response data to state "posts"
-		setPostingan(dataPostingan);
+        //panggil method "fetchData"
+        fetchData();
 
-		console.log(postingan);
-	};
-	return <></>;
+    }, []);
 
+    //function "fetchData"
+    const fetchData = async () => {
+        //fetching
+        const response = await axios.get('http://localhost:3000/api/posts');
+        //get response data
+        const data = await response.data.data;
+
+        //assign response data to state "posts"
+        setPosts(data);
+    }
+
+    return (
+        <Container className="mt-3">
+            <Row>
+                <Col md="{12}">
+                    <Card className="border-0 rounded shadow-sm">
+                        <Card.Body>
+                            <Button as={Link} to="/posts/create" variant="success" className="mb-3">TAMBAH POST</Button>
+                            <Table striped bordered hover className="mb-1">
+                                <thead>
+                                    <tr>
+                                        <th>NO.</th>
+                                        <th>TITLE</th>
+                                        <th>CONTENT</th>
+                                        <th>AKSI</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    { posts.map((post, index) => (
+                                        <tr key={ post.id }>
+                                            <td>{ index + 1 }</td>
+                                            <td>{ post.title }</td>
+                                            <td>{ post.content }</td>
+                                            <td className="text-center"></td>
+                                        </tr>
+                                    )) }
+                                </tbody>
+                            </Table>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default IndexPost;
