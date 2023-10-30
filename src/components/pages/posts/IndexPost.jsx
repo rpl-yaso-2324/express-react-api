4e0d7bbd0a524dbf87ffaf289a61380893b44410
nazwakito <nazwaleony77@gmail.com>
2023-10-30 16:09:03 +0700

    commit index post

--- src/components/pages/posts/IndexPost.jsx
+++ src/components/pages/posts/IndexPost.jsx
@@ -8,7 +8,7 @@ import axios from 'axios';
 
 function IndexPost() {
   //define state
-	const [postingan, setPostingan] = useState([]);
+	const [postingan, setPostingan] = useState([]); 
 
 	//useEffect hook
 	useEffect(() => {
