e1f8134b72bef27fe7ac29318222c6879f88a1cf
cicicisilia <cicicisilia4@gmail.com>
2023-10-31 14:54:12 +0700

    indexPosts

--- src/component/pages/posts/IndexPost.jsx
+++ src/component/pages/posts/IndexPost.jsx
@@ -1,31 +1,65 @@
 import { useState, useEffect } from 'react';
-//import { Link } from "react-router-dom";
-//import { Card, Container, Row, Col, Button, Table } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import { Card, Container, Row, Col, Button, Table } from 'react-bootstrap';
 import axios from 'axios';
 
 function IndexPosts() {
-    const [postingan, setPostingan] = useState([]);
+     //define state
+     const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-       
-        fetchData();
+     //useEffect hook
+     useEffect(() => {
+ 
+         //panggil method "fetchData"
+         fectData();
+ 
+     }, []);
+ 
+     //function "fetchData"
+     const fectData = async () => {
+         //fetching
+         const response = await axios.get('http://localhost:3000/api/posts');
+         //get response data
+         const data = await response.data.data;
+ 
+         //assign response data to state "posts"
+         setPosts(data);
+     };
 
-    }, []);
-
-    const fetchData = async () => {
-
-        const response = await axios.get("http://localhost:3000/api/postingan");
-
-        const dataPostingan = await response.data.postingan;
-
-        setPostingan(dataPostingan);
-
-        console.log(postingan);
-    };
-
-    return <></>
-
-
+    return (
+        <Container className="mt-3">
+            <Row>
+                <Col md="{12}">
+                    <Card className="border-0 rounded shadow-sm">
+                        <Card.Body>
+                            <Button as={Link} to="/posts/create" variant="success" className="mb-3">TAMBAH POST</Button>
+                            <Table striped bordered hover className="mb-1">
+                                <thead>
+                                    <tr>
+                                        <th>NO.</th>
+                                        <th>TITLE</th>
+                                        <th>CONTENT</th>
+                                        <th>AKSI</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    { posts.map((post, index) => (
+                                        <tr key={ post.id }>
+                                            <td>{ index + 1 }</td>
+                                            <td>{ post.title }</td>
+                                            <td>{ post.content }</td>
+                                            <td className="text-center"></td>
+                                        </tr>
+                                    )) }
+                                </tbody>
+                            </Table>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
+
 
 export default IndexPosts;
