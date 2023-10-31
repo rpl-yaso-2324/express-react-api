3239bcc4e8a24a47e802ea6512ab03ed8f15815d
airizen <arinaanggraenitgs@gmail.com>
2023-10-31 07:40:37 +0700

    instal axios dan mengubah lainnya

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -12,4 +12,3 @@ function Layout({ children }) {
 }
 
 export default Layout;
-
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -9,8 +9,18 @@ function Home() {
                     <Card className="border-0 rounded shadow-sm">
                         <Card.Body className="p-4">
                         <h1>EXPRESS.JS + VUE.JS</h1>
-                        <p class="lead">Tutorial FullStack Express.js dan React.js oleh <strong>SantriKoding.com</strong></p>
-                        <Button href="http://santrikoding.com" target="_blank" variant="primary" size="lg">SELENGKAPNYA</Button>
+                        <p class="lead">
+                            Tutorial FullStack Express.js dan React.js oleh 
+                            <strong>SantriKoding.com</strong>
+                        </p>
+                        <Button 
+                        href="http://santrikoding.com" 
+                        target="_blank" 
+                        variant="primary" 
+                        size="lg"
+                        >
+                            SELENGKAPNYA
+                        </Button>
                         </Card.Body>
                     </Card>
                 </Col>
