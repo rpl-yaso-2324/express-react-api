21eddec753894ab0489464627cf3f81e910a1519
rally <rallyaul25@gmail.com>
2023-11-01 13:22:09 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layouts/Layout";
+import IndexPosts from "./components/pages/posts/indexPosts";
+import CreatePosts from "./components/pages/posts/CreatePosts";
+import EditPosts from "./components/pages/posts/EditPosts";
+import Home from "./components/pages/home";
+
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Layout />}></Route>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Home/>} />
+					<Route path="posts" element={<IndexPosts  />}/>
+					<Route path="posts/Create" element={<CreatePosts />}/>
+					<Route path="posts/Edit/:id" element={<EditPosts  />}/>
+				</Route>
 			</Routes>
 		</BrowserRouter>
 	);
