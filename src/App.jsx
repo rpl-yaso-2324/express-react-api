1e24276dad9ae35426d0d9395a7aa855e6d8dcc3
marsyapacarjimin <marsyapacarjimin@gmail.com>
2023-11-01 19:09:15 +0700

    mengubah ukuran text

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from './assets/component/layouts/Layout';
+import Layout from './component/layouts/Layout';
+import Home from "./component/posts/Home";
+import CreatePost from "./component/posts/pages/CreatePost";
+import EditPost from "./component/posts/pages/EditPost";
+import IndexPost from "./component/posts/pages/IndexPost";
+import Layout from "./component/layouts/Layout";
 
-  function App() {
+function App() {
   return (
-  <BrowserRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
         </Route>
       </Routes>
     </BrowserRouter>
-  );
- }
+  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Home />} />
+					<Route path="/posts" element={<IndexPost />} />
+					<Route path="/posts/create" element={<CreatePost />} />
+					<Route path="/posts/edit/:id" element={<EditPost />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
+}
 
 export default App;
