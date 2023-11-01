05e061380b487e8f3599291359e2e413c408891d
juan <juanikbalpratama@gmail.com>
2023-11-01 12:01:20 +0700

    atos

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Layout from './components/layout/layout';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/home';
+import IndexPost from './pages/posts/IndexPost';
+import CreatePosts from './pages/posts/CreatePosts';
+import EditPosts from './pages/posts/Editposts';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home/>}/>
+          <Route path='/posts' element={<IndexPost/>}/>
+          <Route path='/posts/create' element={<CreatePosts/>}/>
+          <Route path='/posts/edit/:id' element={<EditPosts/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
