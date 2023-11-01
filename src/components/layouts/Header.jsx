65d23a92072fab8accf48563a9db8e35ef276d65
rabbanisan <bani.ramadhan99@gmail.com>
2023-11-01 11:58:26 +0700

    fix error, show data indexPost , add createPost

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -2,28 +2,25 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function Header(params) {
-	return (
-		<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-			<Container>
-				<Navbar.Brand to="/">EXPRESS.JS + REACT.JS</Navbar.Brand>
-				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-				<Navbar.Collapse id="responsive-navbar-nav">
-					<Nav className="me-auto">
-						<Nav.Link>
-							<Link to="/" className="nav-link">
-								HOME
-							</Link>
-						</Nav.Link>
-						<Nav.Link>
-							<Link to="/posts" className="nav-link">
-								POSTS
-							</Link>
-						</Nav.Link>
-					</Nav>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
-	);
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand to="/">EXPRESS.JS + REACT.JS</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Link to="/" className="nav-link">
+              HOME
+            </Link>
+
+            <Link to="/posts" className="nav-link">
+              POSTS
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Header;
