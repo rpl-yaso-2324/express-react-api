14bbfdf46876595f352a9b5626fde1779ddbf375
rally <rallyaul25@gmail.com>
2023-11-01 13:26:03 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { Outlet } from "react-router-dom";
-import Header from "./Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layouts/layout";
+import Home from "./components/pages/Home";
+import IndexPost from "./components/pages/posts/Indexpost";
+import EditPost from "./components/pages/posts/Editpost";
+import CreatePost from "./components/pages/posts/CreatePots";
 
-function Layout({ children }) {
-    return (
-        <>
-        <Header />
-        {children}
-        <Outlet />
-        </>
-    );
+function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+				    <Route index element={<Home/>} />
+			        <Route path="/posts" element={<IndexPost />} />
+				    <Route path="/posts/Create" element={< CreatePost/>}/>
+					<Route path="/posts/Edit/:id" element={< EditPost/>}/>
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-export default Layout;
+export default App;
