46efcc8446278b2842bc72054a80c12de584d8df
F18R10 <ramadhanfauzi932@gmail.com>
2023-11-03 11:07:29 +0700

    update

--- src/components/pages/posts/Editpost.jsx
+++ src/components/pages/posts/Editpost.jsx
@@ -1,19 +1,20 @@
 import { Card, Container, Row, Col } from 'react-bootstrap'
 
 function EditPost() {
-    return (
-        <Container className="mt-3">
-            <Row>
-                <Col md="{12}">
-                    <Card className="border-0 rounded shadow-sm">
-                        <Card.Body>
-                            HALAMAN EDIT POST
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
-    );
+    return
+    //(
+    //     <Container className="mt-3">
+    //         <Row>
+    //             <Col md="{12}">
+    //                 <Card className="border-0 rounded shadow-sm">
+    //                     <Card.Body>
+    //                         HALAMAN EDIT POST
+    //                     </Card.Body>
+    //                 </Card>
+    //             </Col>
+    //         </Row>
+    //     </Container>
+    // );
 }
 
 export default EditPost;
