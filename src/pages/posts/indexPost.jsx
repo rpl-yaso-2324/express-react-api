2b141540a9d2b77437afe49504c2324168f18764
Rmt_47 <eighteenofmarch07@gmail.com>
2023-11-07 07:53:45 +0700

    tadi pake akun orang

--- src/pages/posts/indexPost.jsx
+++ src/pages/posts/indexPost.jsx
@@ -46,18 +46,18 @@ function IndexPost() {
 							<Button
 								as={Link}
 								to="/posts/create"
-								variant="warning"
 								className="mb-3"
+								style={{ background: 'linear-gradient(to right, rgba(255, 192, 203, 0.9), rgba(138, 43, 226, 0.9))' }}
 							>
 								TAMBAH POST
 							</Button>
 							<Table striped bordered hover className="mb-1">
 								<thead>
 									<tr>
-										<th>NO.</th>
-										<th>TITLE</th>
-										<th>CONTENT</th>
-										<th>AKSI</th>
+										<th className="text-primary">NO.</th>
+										<th  className="text-success">TITLE</th>
+										<th className="text-warning">CONTENT</th>
+										<th className="text-danger">ACTION</th>
 									</tr>
 								</thead>
 								<tbody>
